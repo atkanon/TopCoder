ceafab35202e3283378bff3b03e3472579aeb931
Takanori Matsumoto <matscube@gmail.com>
2013-03-20 03:49:12 +0900

    SRM573 div1 medium

---
+++ SRM573/SkiResorts.cpp
@@ -0,0 +1,88 @@
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <map>
+#include <utility>
+#include <set>
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <complex>
+#include <stack>
+#include <queue>
+using namespace std;
+static const double EPS = 1e-9;
+typedef long long LL;
+
+class SkiResorts {
+	public:
+	long long minCost(vector <string> road, vector <int> altitude)  {
+		priority_queue<pair<LL, pair<int, int> > > pq;
+		int sz = altitude.size();
+		for (int i = 0; i < sz; i++) pq.push(make_pair(-abs(altitude[0] - altitude[i]), make_pair(0, i)));
+
+		int used[51][51] = {0};
+		while (!pq.empty()) {
+			LL cost = -pq.top().first;
+			int p = pq.top().second.first;
+			int to = pq.top().second.second;
+			pq.pop();
+
+			if (p == sz - 1) return cost;
+
+			if (used[p][to]) continue;
+			used[p][to] = 1;
+
+			for (int i = 0; i < sz; i++) {
+				for (int j = 0; j < sz; j++) {
+					if (road[i][p] == 'N') continue;
+					if (altitude[j] > altitude[to]) continue;
+					if (used[i][j]) continue;
+					pq.push(make_pair(-cost - abs(altitude[i] - altitude[j]), make_pair(i, j)));
+				}
+			}
+		}
+		return -1;
+	}
+
+	
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"NYN",
+ "YNY",
+ "NYN"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {30, 20, 10}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 0LL; verify_case(0, Arg2, minCost(Arg0, Arg1)); }
+	void test_case_1() { string Arr0[] = {"NY",
+ "YN"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {10, 20}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 10LL; verify_case(1, Arg2, minCost(Arg0, Arg1)); }
+	void test_case_2() { string Arr0[] = {"NYN",
+ "YNN",
+ "NNN"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {573, 573, 573}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = -1LL; verify_case(2, Arg2, minCost(Arg0, Arg1)); }
+	void test_case_3() { string Arr0[] = {"NNYNNYYYNN",
+ "NNNNYNYNNN",
+ "YNNNNYYNNN",
+ "NNNNNNYNYY",
+ "NYNNNNNNYY",
+ "YNYNNNNYNN",
+ "YYYYNNNYNN",
+ "YNNNNYYNNN",
+ "NNNYYNNNNN",
+ "NNNYYNNNNN"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {7, 4, 13, 2, 8, 1, 8, 15, 5, 15}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); long long Arg2 = 12LL; verify_case(3, Arg2, minCost(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+// BEGIN CUT HERE 
+int main()  {
+	SkiResorts ___test;
+	___test.run_test(-1);
+}
+// END CUT HERE
